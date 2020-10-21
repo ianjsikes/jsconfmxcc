f1ff8c7669db98e25193664d81a3fcf17217ca8b
Ian J Sikes <ianjsikes@gmail.com>
2020-10-21 07:15:23 -0700

    Something moving

--- src/Shape.js
+++ src/Shape.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useFrame } from 'react-three-fiber'
 
 export const Shape = ({ position = [0, 0, 0] }) => {
+  const meshRef = React.useRef()
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime()
+    meshRef.current.position.y = time
+  })
+
   return (
-    <mesh position={position} castShadow receiveShadow>
+    <mesh ref={meshRef} position={position} castShadow receiveShadow>
       <sphereBufferGeometry args={[0.5, 32, 32]} />
       <meshStandardMaterial color="green" />
     </mesh>
