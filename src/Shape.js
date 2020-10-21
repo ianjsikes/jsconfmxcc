f3cd3ca7c9d84410b636a3df26926301b9e72c5b
Ian J Sikes <ianjsikes@gmail.com>
2020-10-21 07:17:13 -0700

    Random offset to motion

--- src/Shape.js
+++ src/Shape.js
@@ -3,10 +3,11 @@ import { useFrame } from 'react-three-fiber'
 
 export const Shape = ({ position = [0, 0, 0] }) => {
   const meshRef = React.useRef()
+  const factor = React.useMemo(() => 0.5 * Math.random(), [])
 
   useFrame((state) => {
     const time = state.clock.getElapsedTime()
-    const t = Math.sin(time)
+    const t = Math.sin(time * factor)
     meshRef.current.position.y = t
   })
 
